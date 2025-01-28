d878972721ee02dc7161530aa1568d2e41021260
Sytra <phoenixwshadow@gmail.com>
2025-01-28 06:16:04 +0300

    .

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -28,6 +28,7 @@
                 {
                     Console.Write("İşlemi giriniz(+,-,*,/): ");
                     string input = Console.ReadLine();
+
                     islem = input switch
                     {
                         "+" => Islemler.topla,
@@ -36,21 +37,21 @@
                         "/" => Islemler.bol,
                         _ => throw new InvalidOperationException("Geçersiz işlem.")
                     };
+
                     giris = true;
                 }
                 catch(Exception e) 
                 {
                     Console.WriteLine($"Hata: {e.Message}\n");
                 }
-            }
-            if (giris)
-            {
-                SayiGir();
+                if (giris)
+                {
+                    SayiGir();
+                }
             }
         }
-        
 
-        public static int[] Sayi = new int[2];
+        public static double[] Sayi = new double[2];
         public static void SayiGir()
         {
             for (int i = 0; i < 2; i++)
@@ -65,7 +66,7 @@
                         Sayi[i] = Convert.ToInt16(Console.ReadLine());
                         validNumber = true;
                     }
-                    catch 
+                    catch
                     {
                         Console.WriteLine("Sayı veri türünde değer girilmedi, lütfen tekrar deneyiniz.\n");
                     }
@@ -80,12 +81,10 @@
             public static double sonuc;
             public static void Kontrol()
             {
-                int sayi1 = Giris.Sayi[0];
-                int sayi2 = Giris.Sayi[1];
+                double sayi1 = Sayi[0];
+                double sayi2 = Sayi[1];
 
-                Console.WriteLine($"\nGirilen işlem:{islem}");
-                
-                switch (islem)
+                switch(islem)
                 {
                     case Islemler.topla:
                         sonuc = sayi1 + sayi2;
@@ -97,7 +96,15 @@
                         sonuc = sayi1 * sayi2;
                         break;
                     case Islemler.bol:
-                        sonuc = sayi1 / sayi2;
+                        if (sayi2 > 0)
+                        {
+                            sonuc = sayi1 / sayi2;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Bölme işleminde 2. sayı 0 olamaz.\n");
+                            SayiGir();
+                        }
                         break;
                 }
 
@@ -109,7 +116,8 @@
         {
             public static void ConCIkıs()
             {
-                Console.WriteLine ("Sonucunuz: " + Islem.sonuc);
+                Console.WriteLine($"\nGirilen işlem: {islem}");
+                Console.WriteLine ($"Sonucunuz: {Islem.sonuc} ");
 
                 YenidenBasla.YBasla();
             }
@@ -119,8 +127,7 @@
         {
             public static void YBasla()
             {
-                Console.Write("\nTekrar başlamak istiyorsanız 'Enter', Çıkmak için Herhangi bir tuşa basınız: ");
-
+                Console.Write("\nYeniden başlamak için 'Enter', Çıkmak için herhangi bir tuşa basınız.");
                 if (Console.ReadKey().Key == ConsoleKey.Enter )
                 {
                     Console.WriteLine("\n");
